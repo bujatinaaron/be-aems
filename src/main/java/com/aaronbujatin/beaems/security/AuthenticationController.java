70d4efdb87076334a57da54e6126923638084eea
bujatinaaron <106457352+bujatinaaron@users.noreply.github.com>
2023-09-23 14:51:11 +0800

    Add files via upload

--- src/main/java/com/aaronbujatin/beaems/security/AuthenticationController.java
+++ src/main/java/com/aaronbujatin/beaems/security/AuthenticationController.java
@@ -2,12 +2,14 @@ package com.aaronbujatin.beaems.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin("*")
 public class AuthenticationController {
 
 
